fb6e9faa437c8f5f5ba999c738c24e1b2fd1b80d
michaelferry25 <g00420185@atu.ie>
2025-04-17 19:40:29 +0100

    Fixed a bug on the landing page where the get started button and the sign in button didnt lead to the respective pages on click

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
